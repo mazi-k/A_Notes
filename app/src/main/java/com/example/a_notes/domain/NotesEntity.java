4bd02b15eae465a2b03b39ee27c72eb5dac432f1
maria <podliza3000@gmail.com>
2021-10-01 11:28:09 +0300

    add new classes

---
+++ app/src/main/java/com/example/a_notes/domain/NotesEntity.java
@@ -0,0 +1,45 @@
+package com.example.a_notes.domain;
+
+public class NotesEntity {
+
+
+    private final int id;
+    private String title;
+    private String content;
+    private String date;
+
+    public NotesEntity(int id, String title, String content, String date) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
---
+++ app/src/main/java/com/example/a_notes/ui/NoteEditActivity.java
@@ -0,0 +1,16 @@
+package com.example.a_notes.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.a_notes.R;
+
+public class NoteEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_edit);
+    }
+}
