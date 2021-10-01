96e7bac4ac1176c07c6a51853f4a0a140ee225b5
maria <podliza3000@gmail.com>
2021-10-01 11:27:06 +0300

    add toolbar and menu

--- app/src/main/java/com/example/a_notes/ui/NotesListActivity.java
+++ app/src/main/java/com/example/a_notes/ui/NotesListActivity.java
@@ -1,14 +1,16 @@
-package com.example.a_notes;
+package com.example.a_notes.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.a_notes.R;
+
+public class NotesListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_notes_list);
     }
 }
